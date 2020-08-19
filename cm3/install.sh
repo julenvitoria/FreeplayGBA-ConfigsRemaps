cade1989f402ea9ab68780928f6c7f83b32bb6ba
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-19 12:23:36 +0200

    Update install.sh

--- cm3/install.sh
+++ cm3/install.sh
@@ -8,9 +8,11 @@ else
         mkdir /home/pi/tmp
 fi
 cd tmp
-#Descargamos y copiamos las configuraciones de los emuladores
+#Download and copy emulators configs
 ~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/emulators
 sudo cp -R emulators /opt/retropie/
-#Descargamos y copiamos las configuraciones de sisyemas, juegos y mapeos
+#Download and copy systems-games configs and remaps
 ~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/configs
 cp -R configs /opt/retropie/
+cd ..
+rm -r tmp
