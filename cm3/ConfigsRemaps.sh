d53939d3fb12eb391a74bfb2c0813e2ba2f77c59
julenvitoria <36037023+julenvitoria@users.noreply.github.com>
2020-08-19 12:37:56 +0200

    Update ConfigsRemaps.sh

--- cm3/ConfigsRemaps.sh
+++ cm3/ConfigsRemaps.sh
@@ -9,9 +9,13 @@ else
 fi
 cd tmp
 #Download and copy emulators configs
+echo "Download and copy emulators configs"
+sleep 2
 ~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/emulators
 sudo cp -R emulators /opt/retropie/
 #Download and copy systems-games configs and remaps
+echo "Download and copy systems-games configs and remaps"
+sleep 2
 ~/scripts/github-downloader.sh https://github.com/julenvitoria/FreeplayGBA-ConfigsRemaps/tree/master/cm3/configs
 cp -R configs /opt/retropie/
 cd ..
